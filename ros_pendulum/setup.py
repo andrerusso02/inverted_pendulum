5b9159baa05d3902614b5487494f094f833a63a1
André Russo <andrerusso02@gmail.com>
2026-06-14 20:25:20 +0200

    bringup motor

--- ros_pendulum/setup.py
+++ ros_pendulum/setup.py
@@ -1,3 +1,4 @@
+from glob import glob
 import os
 
 from setuptools import find_packages, setup
@@ -12,7 +13,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), os.glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf')) + glob(os.path.join('urdf', '*.xacro'))),
+        (os.path.join('share', package_name, 'urdf', 'assets'), glob(os.path.join('urdf', 'assets', '*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
